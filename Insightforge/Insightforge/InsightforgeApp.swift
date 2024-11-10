11ff906c2129c48c43d4a1b81c5ffed9c53f7df7
aleksejzubel <aleksejzube@gmail.com>
2024-11-10 16:33:11 +0300

    basic project setup

---
+++ Insightforge/Insightforge/InsightforgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightforgeApp.swift
+//  Insightforge
+//
+//  Created by Алексей Зубель on 10.11.24.
+//
+
+import SwiftUI
+
+@main
+struct InsightforgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

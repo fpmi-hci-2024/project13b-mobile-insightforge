8b9c755aafd751b4da7158943790e02f2cc49acc
aleksejzubel <aleksejzube@gmail.com>
2024-11-15 00:29:57 +0300

    add auth screen ui

--- Insightforge/Insightforge/PresentationLayer/Scenes/Auth/AuthView.swift
+++ Insightforge/Insightforge/PresentationLayer/Scenes/Auth/AuthView.swift
@@ -9,10 +9,40 @@ import SwiftUI
 
 struct AuthView: View {
     @ObservedObject var viewModel: AuthViewModel
+    @State private var membersCount: Int = 100000
     
     var body: some View {
-        VStack(alignment: .center, spacing: 40) {
-            Text("Auth")
+        VStack(alignment: .center, spacing: 24) {
+            Image(.launchAppIcon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            
+            VStack(alignment: .center, spacing: 24) {
+                Text("Welcome to our world of books! Here, you’ll find the best selections, from classics to the latest releases! Sign up and discover endless literary adventures with personalized recommendations and exclusive discounts for our members.")
+                    .font(.callout)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(6)
+                
+                Image(.users)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack(spacing: .zero) {
+                    Text("There are already ")
+                        .font(.callout)
+                    
+                    Text("\(membersCount)")
+                        .font(.callout)
+                        .foregroundStyle(._108606)
+                    
+                    Text(" of us!")
+                        .font(.callout)
+                }
+            }
+            .padding(.horizontal, 10)
+            
+            
             
             Button(action: {
                 viewModel.showLogin(isLogin: true)
@@ -29,6 +59,7 @@ struct AuthView: View {
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(.horizontal, 24)
         .background() {
             Color(.EBF_2_EB)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
